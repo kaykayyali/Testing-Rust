c416bb5cb584d05372ed9a432c49a1e93c6081da
Kaysser Kayyali <kaykayyali@gmail.com>
2022-04-15 18:54:42 -0400

    Serve up some files

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,20 +1,19 @@
 #[macro_use] extern crate rocket;
-use rocket::http::Status;
-use rocket::response::{content, status};
-use serde_json;
+use rocket::response::{content};
+use rocket::fs::{FileServer, relative};
 use std::fs;
-fn string_to_static_str(s: String) -> &'static str {
-    Box::leak(s.into_boxed_str())
-}
+
 #[get("/")]
 fn json() -> content::Json<&'static str> {
     let path = "./src/config.json";
     let data = fs::read_to_string(path).expect("Unable to read file");
-    let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
-    content::Json(string_to_static_str(data))
+    content::Json(Box::leak(data.into_boxed_str()))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/config", routes![json])
+    let takeoff = rocket::build();
+
+    takeoff.mount("/config", routes![json])
+        .mount("/", FileServer::from(relative!("static")))
 }
